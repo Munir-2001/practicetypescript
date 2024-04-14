895e4b8be9de93993f8f73b01835a3828af0f147
Munir Abbasi <87448868+Munir-2001@users.noreply.github.com>
2024-04-14 22:27:04 +0500

    commit for the files

---
+++ index.ts
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const query = await inquirer.prompt([{
+    message: 'Hi, enter the name',
+    type: 'input',
+    name: 'name',
+
+}]);
+
+console.log(`Hello ${query.name}`);
