6c47cadbff68c5308ddbf7e8deef58ad44d03810
Munir Abbasi <87448868+Munir-2001@users.noreply.github.com>
2024-04-14 22:47:11 +0500

    added the calculator logic here

--- index.ts
+++ index.ts
@@ -1,14 +1,32 @@
 #! /usr/bin/env node
-
 import inquirer from "inquirer";
 
-const query = await inquirer.prompt([{
-    message: 'Hi, enter the name',
-    type: 'input',
-    name: 'name',
+const query = await inquirer.prompt([
+  {
+    message: "Enter number 1",
+    type: "number",
+    name: "firstnum",
+  },
+  {
+    message: "Enter number 2",
+    type: "number",
+    name: "secondnum",
+  },
+  {
+    message: "Which operatuion do you want to perform?",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
+]);
 
-}]);
-
-console.log(`Hello the name is ${query.name}`);
-
-
+if (query.operator === "Addition") {
+  console.log(query.firstnum + query.secondnum);
+} else if (query.operator === "Subtraction") {
+  console.log(query.firstnum - query.secondnum);
+} else if (query.operator === "Multiplication") {
+  console.log(query.firstnum * query.secondnum);
+} else {
+  console.log(query.firstnum / query.secondnum);
+}
+console.log(query);
